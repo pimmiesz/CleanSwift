9f746e0b4a0ff254a98a8d8b0b63771caafd9f54
Patchariya Piyaaromrat <patchariya@Patchariyas-MacBook-Pro.local>
2019-09-03 10:58:14 +0700

    first commit

---
+++ MobilePhoneBuyersGuideCleanSwift/MobileList/MobileListModels.swift
@@ -0,0 +1,21 @@
+//
+//  MobileListModels.swift
+//  MobilePhoneBuyersGuideCleanSwift
+//
+//  Created by Patchariya Piyaaromrat on 2/9/2562 BE.
+//  Copyright (c) 2562 SCB. All rights reserved.
+//
+
+import UIKit
+
+struct MobileList {
+  /// This structure represents a use case
+  struct Something {
+    /// Data struct sent to Interactor
+    struct Request {}
+    /// Data struct sent to Presenter
+    struct Response {}
+    /// Data struct sent to ViewController
+    struct ViewModel {}
+  }
+}
---
+++ MobilePhoneBuyersGuideCleanSwift/MobileList/MobileTableViewCell.swift
@@ -0,0 +1,44 @@
+//
+//  MobileTableViewCell.swift
+//  MobilePhoneBuyersGuide
+//
+//  Created by Patchariya Piyaaromrat on 27/8/2562 BE.
+//  Copyright © 2562 SCB. All rights reserved.
+//
+
+import UIKit
+
+class MobileTableViewCell: UITableViewCell {
+  
+  @IBOutlet weak var descriptionLabel: UILabel!
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var priceLabel: UILabel!
+  @IBOutlet weak var ratingLabel: UILabel!
+  @IBOutlet weak var img: UIImageView!
+  @IBOutlet weak var starBtn: UIButton!
+  var allVc: AllViewController?
+  var isTapped: Bool = false
+  
+  
+  @IBAction func tap(_ sender: UIButton) {
+    isTapped = !isTapped
+    allVc?.addFavorite(cell: self, isFav: isTapped)
+    
+  }
+  func setImageButton (isfav:Bool){
+    let imageStar: UIImage?
+    if isfav {
+      isTapped = isfav
+      imageStar = UIImage(named: "star-tap.png")
+    } else {
+      isTapped = isfav
+      imageStar = UIImage(named: "star.png")
+    }
+    if let imageStar = imageStar {
+      starBtn.setImage(imageStar, for: .normal)
+    }
+  }
+  
+  
+  
+}
